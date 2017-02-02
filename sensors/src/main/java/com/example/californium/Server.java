0c4446f735e4ea4f67a8a8dc7a8c664373e723fc
Maxim Kolchin <kolchinmax@gmail.com>
2017-02-02 09:11:18 +0300

    Set californium-core to 1.0.4 version

--- sensors/src/main/java/com/example/californium/Server.java
+++ sensors/src/main/java/com/example/californium/Server.java
@@ -1,19 +1,16 @@
 package com.example.californium;
 
+import org.eclipse.californium.core.CoapServer;
+import org.eclipse.californium.core.network.CoapEndpoint;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import org.eclipse.californium.core.CoapServer;
-import org.eclipse.californium.core.network.CoAPEndpoint;
 
 public class Server extends CoapServer {
 
     private static final int INITIAL_PORT = 64000;
     private static final int NUMBER_OF_SENSORS = 2;
-//    private final ExecutorService threadPool = Executors.newCachedThreadPool();
     private final Map<Integer, TemperatureSensor> sensors = new HashMap<>();
 
     public Server() {
@@ -23,7 +20,7 @@ public class Server extends CoapServer {
         
         for (int i = 0; i < NUMBER_OF_SENSORS; i++) {
             final int port = INITIAL_PORT + i;
-            addEndpoint(new CoAPEndpoint(port));
+            addEndpoint(new CoapEndpoint(port));
             final TemperatureSensor sensor = new TemperatureSensor(port);
             sensor.setListener(resource);
             sensors.put(port, sensor);
@@ -46,13 +43,6 @@ public class Server extends CoapServer {
         }
         
         super.start();
-        
-//        threadPool.shutdown();
-//        try {
-//            threadPool.awaitTermination(10, TimeUnit.SECONDS);
-//        } catch (InterruptedException ex) {
-//        }
-//        threadPool.shutdownNow();
     }
 
 }
